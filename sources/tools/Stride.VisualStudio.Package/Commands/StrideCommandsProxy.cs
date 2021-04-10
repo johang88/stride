626fd8db87ff8e8ec198f0d28206ca4d6e093d28
Johan Gustafsson <johan@tinyrocket.se>
2021-04-10 19:18:47 +0200

    Fixed stride vsplugin FindStrideSdkDir .net target versions

--- sources/tools/Stride.VisualStudio.Package/Commands/StrideCommandsProxy.cs
+++ sources/tools/Stride.VisualStudio.Package/Commands/StrideCommandsProxy.cs
@@ -173,11 +173,16 @@ namespace Stride.VisualStudio.Commands
             {
                 // Try both net5.0 and net472
                 var success = false;
-                foreach (var framework in new[] { ".NET,Version=v5.0", ".NETFramework,Version=v4.7.2" })
+                foreach (var framework in new[] { ".NETCoreApp,Version=v5.0" })
                 {
                     var logger = new Logger();
                     var solutionRoot = Path.GetDirectoryName(solution);
-                    var (request, result) = await Task.Run(() => RestoreHelper.Restore(logger, NuGetFramework.ParseFrameworkName(framework, DefaultFrameworkNameProvider.Instance), "win", packageName, new VersionRange(packageInfo.ExpectedVersion.ToNuGetVersion()), solutionRoot));
+                    
+                    var nugetFramework = NuGetFramework.ParseFrameworkName(framework, DefaultFrameworkNameProvider.Instance);
+                    nugetFramework = new NuGetFramework(nugetFramework.Framework, nugetFramework.Version, "windows", new Version("7.0"));
+
+                    var (request, result) = await Task.Run(() => RestoreHelper.Restore(logger, nugetFramework, "win", packageName, new VersionRange(packageInfo.ExpectedVersion.ToNuGetVersion()), solutionRoot));
+
                     if (result.Success)
                     {
                         packageInfo.SdkPaths.AddRange(RestoreHelper.ListAssemblies(result.LockFile));
